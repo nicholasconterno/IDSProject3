c5bdaec4d057dc703e641287b6ce5a4aa7bd8090
nicholasconterno <77944074+nicholasconterno@users.noreply.github.com>
2023-11-15 20:33:44 +0000

    f

--- main.py
+++ main.py
@@ -1,57 +1,35 @@
-import pandas as pd
-import seaborn as sns
-def load_data(url):
-    df = pd.read_csv(url, skiprows=range(1, 32))  # Adjust the range as needed
-    return df
+import requests
+import os
+import dotenv
 
-def get_summary_statistics(df):
-    return df.describe()
+# Load the environment variables
+dotenv.load_dotenv()
 
-def get_mean(df, column_name):
-    return df[column_name].mean()
+def run(access_token, job_id, server_host_name):
+    """
+    Trigger the Databricks NBA data pipeline job run
+    """
+    # Setup and make the api request
+    url = f"https://{server_host_name}/api/2.0/jobs/run-now"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+    data = {"job_id": job_id}
+    response = requests.post(url, headers=headers, json=data, timeout=100)
 
-def get_median(df, column_name):
-    return df[column_name].median()
+    # Check the response status code
+    if response.status_code == 200:
+        print("Databricks job run successfully triggered....")
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
 
-def get_stdev(df, column_name):
-    return df[column_name].std()
-
-import matplotlib.pyplot as plt
-
-def plot_histogram_save(df, column_name, filename='histogram.png'):
-    # Using seaborn styles
-    sns.set_style("whitegrid")
-    
-    # Create the histogram
-    plt.figure(figsize=(10, 6))  # Set figure size for larger plot
-    sns.histplot(df[column_name], kde=True, color="dodgerblue", bins=30)  # KDE shows density curve
-    plt.title(f'Histogram of {column_name}', fontsize=18)
-    plt.xlabel(column_name, fontsize=14)
-    plt.ylabel('Density', fontsize=14)
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-    
-    # Save the figure
-    plt.tight_layout()  # This ensures that the labels don't get cut off
-    plt.savefig(filename)
-    plt.close()  # Close the figure to free up memory
-    return filename
+    return response.status_code
 
 
-url = 'https://gist.githubusercontent.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea/raw/0c794a9717f18b094eabab2cd6a6b9a226903577/movies.csv'
-df = load_data(url)
-
-# Print summary statistics
-print(get_summary_statistics(df))
-
-md = df.to_markdown()
-with open('generated_markdown.md','w') as f:
-    f.write(md)
-# Assuming the dataset has a column named 'rating' 
-# (adjust column names based on the actual dataset columns)
-print(f"Mean of 'Rotten Tomatoes %': {get_mean(df, 'Rotten Tomatoes %')}")
-print(f"Median of 'Rotten Tomatoes %': {get_median(df, 'Rotten Tomatoes %')}")
-print(f"Standard Deviation of 'Rotten Tomatoes %': {get_stdev(df, 'Rotten Tomatoes %')}")
-
-# Plotting a histogram for 'rating'
-fname= plot_histogram_save(df, 'Rotten Tomatoes %')
+if __name__ == "__main__":
+    db_access_token = os.getenv("PAT")
+    db_job_id = os.getenv("JOB_ID")
+    db_server_host_name = 'adb-4253147117925923.3.azuredatabricks.net'
+    print(db_server_host_name)
+    run(db_access_token, db_job_id, db_server_host_name)
--- test_main.py
+++ test_main.py
@@ -1,24 +1,41 @@
-
-from main import load_data, get_mean, get_median,get_stdev,get_summary_statistics
 import unittest
-class TestDataMethods(unittest.TestCase):
+from unittest.mock import patch
+from main import run
+import dotenv
+import os
+# Load the environment variables
+dotenv.load_dotenv()
+class TestMainScript(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.url = 'https://gist.githubusercontent.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea/raw/0c794a9717f18b094eabab2cd6a6b9a226903577/movies.csv'
-        cls.df = load_data(cls.url)
+    @patch('main.requests.post')
+    def test_run_success(self, mock_post):
+        # Mock the response object
+        mock_response = mock_post.return_value
+        mock_response.status_code = 200
 
-    def test_get_mean(self):
-        mean_rating = get_mean(self.df, 'Rotten Tomatoes %')
-        self.assertTrue(isinstance(mean_rating, float))
+        # Call the run function with mock data
+        result = run("mock_token", "mock_job_id", "mock_server_host_name")
 
-    def test_get_median(self):
-        median_rating = get_median(self.df, 'Rotten Tomatoes %')
-        self.assertTrue(isinstance(median_rating, float))
+        # Check if the function returns the expected result
+        self.assertEqual(result, 200)
 
-    def test_get_stdev(self):
-        stdev_rating = get_stdev(self.df, 'Rotten Tomatoes %')
-        self.assertTrue(isinstance(stdev_rating, float))
+    @patch('main.requests.post')
+    def test_run_failure(self, mock_post):
+        # Mock the response object
+        mock_response = mock_post.return_value
+        mock_response.status_code = 400
+        mock_response.text = "Error message"
 
+        # Call the run function with mock data
+        result = run("mock_token", "mock_job_id", "mock_server_host_name")
+
+        # Check if the function returns the expected result
+        self.assertEqual(result, 400)
+
+#test that .env file is loaded
+    def test_env_file(self):
+        assert (os.getenv("PAT") != None)
+        assert (os.getenv("JOB_ID") != None)
+        assert (os.getenv("HOST") != None)
 if __name__ == '__main__':
     unittest.main()
