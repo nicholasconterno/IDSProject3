3a098df4469d61d83b641a7efed6b2bd174b4358
nicholasconterno <nicholasconterno>
2023-11-16 03:12:59 +0000

    c

--- src/Transform_Load_data.py
+++ src/Transform_Load_data.py
@@ -1,55 +1,81 @@
 # Databricks notebook source
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 from pyspark.sql.utils import AnalysisException
+
 # Initialize a SparkSession
 spark = SparkSession.builder \
-    .appName("Transform Delta Table") \
+    .appName("Transform Delta Table with Spark SQL") \
     .getOrCreate()
+
 # Define the path for the original Delta table in DBFS
 original_table_path = 'dbfs:/dbfs/mnt/delta/baseball_delta_table'
+
 try:
     # Read the original Delta table into a Spark DataFrame
     original_df = spark.read.format("delta").load(original_table_path)
     if original_df.rdd.isEmpty():
         raise ValueError("The DataFrame is empty. Check your data source.")
 
-    # Cast the 'id' column to the desired data type (e.g., IntegerType)
-    original_df = original_df.withColumn("id", col("id").cast("integer"))
-    original_df = original_df.withColumn("year", col("year").cast("integer"))
-    original_df = original_df.withColumn("stint", col("stint").cast("integer"))
+    # Register DataFrame as a temporary view
+    original_df.createOrReplaceTempView("original_data")
 
-    # Select the relevant columns for player information
-    player_info_df = original_df.select("id", "player", "year",
-                                        "stint", "team", "lg").distinct()
+    # Using Spark SQL to transform the data
+    transformed_df = spark.sql("""
+        SELECT 
+            CAST(id AS INTEGER) AS id, 
+            player, 
+            CAST(year AS INTEGER) AS year, 
+            CAST(stint AS INTEGER) AS stint, 
+            team, 
+            lg
+        FROM original_data
+    """)
 
     # Define the path for the new Delta table for player information
     player_info_table_path = "/dbfs/mnt/delta/updated_player_info"
 
-    # Write the DataFrame to a Delta table for player information
-    player_info_df.write.format("delta").mode("overwrite").\
-        save(player_info_table_path)
+    # Write the transformed DataFrame to a Delta table for player information
+    transformed_df.write.format("delta").mode("overwrite").save(player_info_table_path)
 
     # Show the first few rows of the player information Delta table
-    player_info_df.show()
-    original_df = original_df.withColumn("id", col("id").cast("long"))
-    original_df = original_df.withColumn("year", col("year").cast("long"))
-    original_df = original_df.withColumn("stint", col("stint").cast("long"))
-    # Cast the 'id' column to the desired data type (e.g., Integ
-    game_stats_df = original_df.select(
-        "id", "year", "team",  "r", "h", "X2b", "X3b", "hr", "rbi",
-        "sb", "cs", "bb", "so", "ibb", "hbp", "sh", "sf", "gidp"
-    ).distinct()
+    transformed_df.show()
+
+    # Register transformed DataFrame as a temporary view for further transformation
+    transformed_df.createOrReplaceTempView("transformed_data")
+
+    # Perform additional transformations using Spark SQL
+    game_stats_df = spark.sql("""
+        SELECT 
+            id, 
+            year, 
+            team, 
+            r, 
+            h, 
+            X2b, 
+            X3b, 
+            hr, 
+            rbi,
+            sb, 
+            cs, 
+            bb, 
+            so, 
+            ibb, 
+            hbp, 
+            sh, 
+            sf, 
+            gidp
+        FROM transformed_data
+    """)
 
     # Define the path for the Delta table for game statistics
     game_stats_table_path = "/dbfs/mnt/delta/game_stats"
 
     # Write the DataFrame to a Delta table for game statistics
-    game_stats_df.write.format("delta").mode("overwrite").\
-        save(game_stats_table_path)
+    game_stats_df.write.format("delta").mode("overwrite").save(game_stats_table_path)
 
     # Show the first few rows of the game statistics Delta table
     game_stats_df.show()
+
 except AnalysisException as e:
     # Handle exceptions related to DataFrame operations
     print(f"An error occurred during DataFrame operations: {e}")
@@ -57,3 +83,4 @@ except AnalysisException as e:
 except Exception as e:
     # Handle any other exceptions
     print(f"An error occurred: {e}")
+
